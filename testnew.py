b4ed161166ea1ac9ecd5f48ca19cab17bf1c00bb
minhhi227 <minhhi227@gmail.com>
2023-08-02 08:53:40 -0400

    changes

---
+++ testnew.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pandas as pd
+# from binance.client import Client
+# import ta
+import matplotlib.pyplot as plt
+
+# client = Client()
+
+# klinesT = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_1HOUR, "01 january 2017")
